49d4911ebd2c17e55e579b66b87c3ed292e1e968
Adilson Angelo <aasj2@cin.ufpe.br>
2018-07-01 14:42:03 -0300

    Controller fix

--- src/main/java/br/ufpe/cin/flixcrawlerdata/controller/PaginaController.java
+++ src/main/java/br/ufpe/cin/flixcrawlerdata/controller/PaginaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufpe.cin.flixcrawlerdata.DAO.Pagina;
@@ -19,38 +20,39 @@ import br.ufpe.cin.flixcrawlerdata.service.PaginaService;
 @RestController
 @RequestMapping("/pagina")
 public class PaginaController  {
-	
+
 	@Autowired
 	private PaginaService ps;
-	
+
 	@PostMapping("/new")
 	public Pagina savePagina(@RequestBody Pagina pagina) {
 		return this.ps.create(pagina);
 	}
 
-	@GetMapping("/id/{id}")
-	public Pagina getPagina(@PathVariable long id) {
+	@GetMapping(params = {"id"})
+	public Pagina getPaginaById(@RequestParam("id") Long id) {
 		return this.ps.getById(id);
 	}
-	
-	@GetMapping("/url/{url}")
-	public Pagina getPaginaUrl(@PathVariable String url) throws UnsupportedEncodingException {
+
+
+	@GetMapping(params = {"url"})
+	public Pagina getPaginaByUrl(@RequestParam("url") String url) throws UnsupportedEncodingException {
 		return this.ps.getByUrl(URLDecoder.decode(url, "UTF-8"));
 	}
-	
+
 	@GetMapping("/count")
 	public Long getPaginaCount(){
 		return this.ps.count();
 	}
-	
+
 	@DeleteMapping("/delete/{id}")
 	public Pagina deletePagina(@PathVariable long id) {
 		return this.ps.delete(id);
 	}
-	
+
 	@GetMapping("/domain-ids/{domain}")
-	public List<Long> getByDomain(@PathVariable String domain){
-		return this.ps.getIdsByDomain(domain);
+	public List<Long> getByDomain(@PathVariable String domain) throws UnsupportedEncodingException{
+		return this.ps.getIdsByDomain(URLDecoder.decode(domain, "UTF-8"));
 	}
-	
+
 }
